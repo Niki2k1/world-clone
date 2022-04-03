71af48377d77f7e925d3b3c5848c4e40f2ac5766
Niklas Lausch <support@niki2k1.de>
2022-04-03 20:08:58 +0200

    Changed building/devtools to vite added random wordlist

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,6 +1,9 @@
+import { german } from '@/assets/wordlist.json'
+
 export default class Dictionary {
   static getRandomWord () {
-    const word = 'apfel'
+    const randomIndex = Math.floor(Math.random() * german.length)
+    const word = german[randomIndex]
     return word.toUpperCase()
   }
 }
